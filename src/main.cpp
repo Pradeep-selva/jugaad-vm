915e89402ac97a9836bc6bd3a59b22b609e1b434
pradeep-selva <pradeepselvaraju@gmail.com>
2022-10-26 21:48:01 +0530

    ft: read code and parse to execute

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "instructions.h"
 
 #include<bits/stdc++.h>
+#include <stdexcept>
+
 using namespace std;
 
 void traverse_stack(stack<int> & st) {
@@ -13,27 +15,44 @@ void traverse_stack(stack<int> & st) {
     st.push(x);
 }
 
+void tokenize(string const &str, vector<string> &out) {
+    stringstream ss(str);
+    string word;
+
+    while (ss >> word) {
+        out.push_back(word);
+    }
+}
+
 int main() {
     stack<int> vm;
 
-    cout<<"0 1"<<endl;
-    cout<<"0 2"<<endl;
-    cout<<"1"<<endl;
-    cout<<"---------"<<endl;
+    cout<<"Enter bytecode to execute on stack VM (type 'exit' to exit)-"<<endl;
 
-    struct Instruction push = find_instruction_by_opcode(0);
-    struct Instruction add = find_instruction_by_opcode(1);
+    string line;
+    while (getline(cin, line) && line != "exit") {
+        vector<string> tokens;
+        tokenize(line, tokens);
 
-    push.execute(1, vm);
-    push.execute(2, vm);
+        if(tokens.size() > 2 || tokens.size() < 1)
+            throw invalid_argument("[PARSER] INVALID NUMBER OF TOKENS RECEIVED");
 
-    cout<<"INITIAL STATE\n";
-    traverse_stack(vm);
+        int opcode = stoi(tokens.at(0));
+        int operand = stoi(tokens.size() == 2 ? tokens.at(1): "0");
 
-    cout<<endl<<endl;
+        struct Instruction instruction = find_instruction_by_opcode(opcode);
+        instruction.execute(operand, vm);
 
-    cout<<"FINAL STATE\n";
-    add.execute(0, vm);
+        cout<<"\n[OPERATION EXEC] "<<instruction.name<<endl;
+    }
+
+    if(vm.size() > 1)
+       throw invalid_argument("[EXECUTOR] COULD NOT RESOLVE TO A SINGLE OUTPUT"); 
+
+    cout<<"\n--------------------"<<endl;
+
+    cout<<"FINAL VM STATE\n";
+    cout<<"--------------------"<<endl;
     traverse_stack(vm);
 
     return 0;
